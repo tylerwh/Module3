b50bc8fb6bfc5c6c44c70643629d4dd840f16b9b
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-08 07:15:39 -0500

    Update average_scores.py
    Added main  to average_scores.py; Created test table for average()

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -6,8 +6,28 @@ Purpose: This program is to demonstrate gathering user input, formatting the dat
 
 
 def average():
-  score1 = input("Score 1: ")
-  score2 = input("Score 2: ")
-  score3 = input("Score 3: ")
+  score1 = float(input("Score 1: "))
+  score2 = float(input("Score 2: "))
+  score3 = float(input("Score 3: "))
   number_of_tests = 3
   return (score1 + score2 + score3) / number_of_tests
+
+
+if __name__ == "__main__":
+    
+    
+    first_name = input("Hello, what is your first name? ").capitalize()
+    last_name = input("Your last name? ").capitalize()
+    age = input("And your age: ")
+    print(f'{first_name}' + ", you are now going to enter the score for each of your last three tests.\n")
+    average_scores = average()
+    print("\n" + f'{last_name}' + ", " + f'{first_name}' + " age : " + f'{age}' + " average score: {0:.2f}".format(average_scores))
+
+
+# The below table are a series of tests run for average()
+
+#      Input      // Expected Output // Actual Output
+#  85, 90, 95     //      90.00      //      90.00
+# 93.3, 92.2, 91.1//      92.20      //      92.20
+#  90, 79.9, 83   //      84.30      //      84.30
+# 90, 90, ninety  //   ValueError    // "ValueError: could not convert string to float: 'ninety'"
